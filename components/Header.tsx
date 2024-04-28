8d2d952c931b4c76ec75f297a8d894e5146a45d9
Manuel Rodriguez <mrodriguez@avantio.com>
2024-04-28 19:40:23 +0200

    refactor(Header): Remove useSearchParams

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -37,13 +36,7 @@ const links: {
 // A header with a logo on the left, links in the center (like Pricing, etc...), and a CTA (like Get Started or Login) on the right.
 // The header is responsive, and on mobile, the links are hidden behind a burger button.
 const Header = () => {
-  const searchParams = useSearchParams();
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  // setIsOpen(false) when the route changes (i.e: when the user clicks on a link on mobile)
-  useEffect(() => {
-    setIsOpen(false);
-  }, [searchParams]);
 
   return (
     <header className="sticky top-0 z-[99] bg-base-100 border-b border-base-content/10">
