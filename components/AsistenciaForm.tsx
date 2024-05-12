7704d78355e805310d75cd26a4c56d5c63a1205c
Manuel Rodriguez <manuelrodriguez@MacBook-Pro-Manu-Rodriguez.local>
2024-05-12 09:00:44 +0200

    refactor(AsistenciaForm): Change form inputs

--- components/AsistenciaForm.tsx
+++ components/AsistenciaForm.tsx
@@ -25,10 +25,11 @@ function AsistenciaForm() {
       </label>
       <label className="form-control w-full">
         <div className="label">
-          <span className="label-text">Opción <b>especial</b> de menú:</span>
+          <span className="label-text">Opción de menú:</span>
         </div>
         <select className="select select-bordered">
-          <option disabled selected>Elige una si la necesitas</option>
+          <option disabled>Elige una si la necesitas</option>
+          <option>Estándar</option>
           <option>Vegetariano</option>
           <option>Vegano</option>
           <option>Celíaco</option>
@@ -51,7 +52,7 @@ function AsistenciaForm() {
         </div>
         <input type="text" className="input input-bordered w-full" />
       </label>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 my-4">
         <p>Hemos decidido que nuestra Luna de miel la vamos a disfrutar en Madagascar e Islas Mauricio.  Como sabéis, son dos lugares que siempre nos han llamado la atención desde que empezamos a mirar destinos y qué mejor momento que ahora para poder cumplir este sueño.</p>
         <p>Sin lugar a dudas, vuestra compañía en nuestro dia mas especial es el mejor regalo, pero si además queréis contribuir de alguna otra forma, podéis hacer aquí:</p>
         <div className="flex flex-col gap-4 mt-2">
