554e49030073cbfc961565b6394887aa6e012bc3
Manuel Rodriguez <manuelrodriguez@MacBook-Pro-Manu-Rodriguez.local>
2024-05-17 15:45:01 +0200

    refactor(Config): update config

--- config.ts
+++ config.ts
@@ -76,13 +76,11 @@ const config = {
     // subdomain to use when sending emails, if you don't have a subdomain, just remove it. Highly recommended to have one (i.e. mg.yourdomain.com or mail.yourdomain.com)
     subdomain: "mg",
     // REQUIRED — Email 'From' field to be used when sending magic login links
-    fromNoReply: `Web de boda - María y Adrián`,
+    fromNoReply: `Web de boda - María y Adrián <noreply@mariaguzmanyadriancalvo.es>`,
     // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
-    fromAdmin: `Web de boda - María y Adrián`,
+    fromAdmin: `Manu en Web de boda - María y Adrián <noreply@mariaguzmanyadriancalvo.es>`,
     // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
-    supportEmail: "",
     // When someone replies to supportEmail sent by the app, forward it to the email below (otherwise it's lost). If you set supportEmail to empty, this will be ignored.
-    forwardRepliesTo: "",
   },
   colors: {
     // REQUIRED — The DaisyUI theme to use (added to the main layout.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
