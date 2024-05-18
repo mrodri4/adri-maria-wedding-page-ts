cee4c354d70a3cdcecd4edb42fa111f2cf5abd36
Manuel Rodriguez <manuelrodriguez@MacBook-Pro-Manu-Rodriguez.local>
2024-05-18 09:27:09 +0200

    refactor(Header): Change logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,10 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-import logo from "@/app/icon.png";
+import logo from "@/public/images/logo.png";
 import config from "@/config";
 
 import { Playfair_Display_SC } from "next/font/google";
-import clsx from "clsx";
 
 const font = Playfair_Display_SC({
   subsets: ["latin"],
@@ -58,9 +57,10 @@ const Header = () => {
             {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" className="w-8 h-8 stroke-secondary">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
             </svg> */}
-            <span className={clsx("text-primary text-3xl", font.className)}>M</span>
+            {/* <span className={clsx("text-primary text-3xl", font.className)}>M</span>
             <span className={clsx("text-primary mt-auto")}>&</span>
-            <span className={clsx("text-primary text-3xl", font.className)}>A</span>
+            <span className={clsx("text-primary text-3xl", font.className)}>A</span> */}
+            <Image src={logo} alt={`${config.appName} logo`} priority={true} width={50} height={50} />
           </Link>
         </div>
         {/* Burger button to open menu on mobile */}
@@ -119,7 +119,6 @@ const Header = () => {
                 src={logo}
                 alt={`${config.appName} logo`}
                 className="w-8"
-                placeholder="blur"
                 priority={true}
                 width={32}
                 height={32}
